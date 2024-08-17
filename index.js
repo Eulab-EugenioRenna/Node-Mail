b80a44ea4da0b81739f43fade573115d79daf016
eulabconsult <eulabconsult@gmail.com>
2024-08-17 23:27:54 +0200

    Pdg Summer Camp

--- index.js
+++ index.js
@@ -10,29 +10,59 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/send-email', (req, res) => {
-  const name = req.body.name;
+  const nome = req.body.nome;
+  const cognome = req.body.cognome;
   const email = req.body.email;
-  const telephone = req.body.telephone;
-  const message = req.body.message;
+  const eta = req.body.eta;
+  const tel1 = req.body.tel1;
+  const tel2 = req.body.tel2;
+  const sesso = req.body.sesso;
+  const prenotazioni = req.body.prenotazioni;
+  const materassino = req.body.materassino;
+  const allergie = req.body.allergie;
+  const altro = req.body.altro;
+  const media = req.body.media;
+  const privacy = req.body.privacy;
+  const bonifico = req.body.bonifico;
 
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'eulabconsult@gmail.com', // Replace with your email address
-      pass: 'pxeuniartpnddedy' // Replace with your email password
-    }
+      pass: 'pxeuniartpnddedy', // Replace with your email password
+    },
   });
 
   const mailOptions = {
     from: 'eulabconsult@gmail.com',
-    to: 'eulabconsult@gmail.com', // Replace with recipient email address
-    subject: 'New Contact Form Submission',
+    to: 'pdellagrazia@gmail.com', // Replace with recipient email address
+    subject: 'Iscrizione Pdg Summer Camp ',
     text: `
-      Name: ${name}
+      Nome: ${nome}
+      Cognome: ${cognome}
       Email: ${email}
-      Telephone: ${telephone}
-      Message: ${message}
-    `
+      Età: ${eta}
+      Sesso: ${sesso}
+      Tel1: ${tel1} 
+      Tel2: ${tel2}
+      
+      Numero Prenotazioni: ${prenotazioni}
+      Altro: ${altro}
+
+      Materassino: ${materassino}
+      Allergie: ${allergie}
+
+      Consenso Media: ${media}
+      Consenso Privcay: ${privacy}
+    `,
+    attachments: [
+      {
+        // encoded string as an attachment
+        filename: 'bonifico.jpg',
+        content: bonifico.split('base64,')[1],
+        encoding: 'base64',
+      },
+    ],
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
